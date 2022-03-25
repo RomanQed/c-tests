5d03ab47e92a5db3bf0bc800008bc4613af8f948
Roman Bakaldin <gbakaldin@gmail.com>
2022-03-25 22:59:41 +0300

    Improve macro array command

--- src/main/java/com/github/romanqed/ctests/macro/ArrayCommand.java
+++ src/main/java/com/github/romanqed/ctests/macro/ArrayCommand.java
@@ -20,21 +20,22 @@ public class ArrayCommand extends MacroCommand {
 
     @Override
     public String execute(List<String> arguments) throws Exception {
-        if (arguments.size() < 2) {
+        if (arguments.size() < 3) {
             throw new IllegalArgumentException("Invalid array command arguments!");
         }
-        MacroCommand type = TYPES.get(arguments.get(0));
+        int length = Integer.parseInt(arguments.get(0));
+        String split = arguments.get(1);
+        MacroCommand type = TYPES.get(arguments.get(2));
         if (type == null) {
             throw new IllegalArgumentException("Invalid array type!");
         }
-        int length = Integer.parseInt(arguments.get(1));
         StringBuilder builder = new StringBuilder();
-        List<String> commandArguments = arguments.subList(2, arguments.size());
+        List<String> commandArguments = arguments.subList(3, arguments.size());
         for (int i = 0; i < length; ++i) {
-            builder.append(type.execute(commandArguments)).append(' ');
+            builder.append(type.execute(commandArguments)).append(split);
         }
         String ret = builder.toString();
-        ret = ret.substring(0, ret.length() - 1);
+        ret = ret.substring(0, ret.length() - split.length());
         return ret;
     }
 }

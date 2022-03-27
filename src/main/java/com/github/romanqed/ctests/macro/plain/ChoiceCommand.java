0db1a95562971aa6e7b03c1fa50077b937133e1c
Roman Bakaldin <gbakaldin@gmail.com>
2022-03-28 00:00:17 +0300

    Add choice macro command Improve macro regex

---
+++ src/main/java/com/github/romanqed/ctests/macro/plain/ChoiceCommand.java
@@ -0,0 +1,21 @@
+package com.github.romanqed.ctests.macro.plain;
+
+import com.github.romanqed.ctests.macro.Array;
+import com.github.romanqed.ctests.macro.MacroCommand;
+import com.github.romanqed.ctests.macro.NamedMacro;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@NamedMacro("che")
+@Array("che")
+public class ChoiceCommand extends MacroCommand {
+    @Override
+    public String execute(List<String> arguments) {
+        if (arguments.isEmpty()) {
+            return "";
+        }
+        int index = ThreadLocalRandom.current().nextInt(0, arguments.size());
+        return arguments.get(index);
+    }
+}
